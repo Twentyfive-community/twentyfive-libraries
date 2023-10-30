f5dcdb2872580662bc25285c6f90de36d6cccee6
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-10-30 12:03:35 +0100

    autocomplete fix

--- projects/twentyfive-autocomplete-sync-async/src/lib/twentyfive-autocomplete-sync-async.module.ts
+++ projects/twentyfive-autocomplete-sync-async/src/lib/twentyfive-autocomplete-sync-async.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TwentyfiveAutocompleteSyncAsyncComponent } from './twentyfive-autocomplete-sync-async.component';
 import { TwentyfiveAutocompleteComponent } from './twentyfive-autocomplete/twentyfive-autocomplete.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 
@@ -10,6 +14,11 @@ import { TwentyfiveAutocompleteComponent } from './twentyfive-autocomplete/twent
     TwentyfiveAutocompleteComponent
   ],
   imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   exports: [
     TwentyfiveAutocompleteSyncAsyncComponent
