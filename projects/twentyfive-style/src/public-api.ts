1d15787ef280d87d7534a804637d178f65fb09d7
twentyfive <twentyfivesrls@gmail.com>
2023-11-28 17:05:21 +0100

    added folder constant

--- projects/twentyfive-style/src/public-api.ts
+++ projects/twentyfive-style/src/public-api.ts
@@ -5,3 +5,8 @@
 export * from './lib/twentyfive-style.service';
 export * from './lib/twentyfive-style.component';
 export * from './lib/twentyfive-style.module';
+
+export * from './constants/genetric-components-themes';
+export * from './constants/empty-function';
+
+
