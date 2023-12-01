cb3ea675917cc1098aff03fe1ffd546c12f89714
Stefano Miceli <stefano.miceli@ext.leroymerlin.it>
2023-12-01 17:12:52 +0100

    Changed style theme imports

--- projects/twentyfive-style/src/public-api.ts
+++ projects/twentyfive-style/src/public-api.ts
@@ -7,6 +7,13 @@ export * from './lib/twentyfive-style.component';
 export * from './lib/twentyfive-style.module';
 
 export * from './lib/genetric-components-themes';
-export * from './lib/constants/empty-function';
+
+export {AlertTheme, ButtonSizeWithIconTheme, CheckboxTheme,
+  DatePickerTheme,DatePickerButtonTheme, InputTheme,
+  LabelTheme, AddonTheme, InputGroupButtonTheme,
+  InputIconTheme, ButtonSizeTheme, ButtonTheme,
+  InputWithIconTheme, RadioButtonTheme, TableTheme,
+  MobileTableTheme, MobileTheadTheme, TableHeadTheme,
+  SelectTheme, SwitchTheme, TextareaTheme, BoxTheme} from './lib/genetric-components-themes';
 
 
