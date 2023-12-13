d37653e8bd8e6845b0a75b92d5ef3b2455607e69
twentyfive <twentyfivesrls@gmail.com>
2023-12-13 15:20:49 +0100

    fixed event emitter on menu-item and sidebar

---
+++ projects/twentyfive-menu-item/src/lib/sidenav-item.ts
@@ -0,0 +1,7 @@
+export class SidenavItem {
+  title?: string | undefined;
+  icon?: string | undefined;
+  navigationUrl?: string | undefined;
+  disableClick?: boolean | undefined;
+  labelColor: string = '';
+}
--- projects/twentyfive-menu-item/src/public-api.ts
+++ projects/twentyfive-menu-item/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/twentyfive-menu-item.service';
 export * from './lib/twentyfive-menu-item.component';
 export * from './lib/twentyfive-menu-item.module';
+export * from './lib/sidenav-item';
