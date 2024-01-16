73c55d3197292081af6786caac54349ff5569740
Twentyfivesrls <twentyfivesrls@gmail.com>
2024-01-16 16:03:07 +0100

    update

--- projects/twentyfive-modal/src/lib/twentyfive-modal.module.ts
+++ projects/twentyfive-modal/src/lib/twentyfive-modal.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {TwentyfiveModalComponent} from './twentyfive-modal.component';
 import {CommonModule, NgClass, NgIf} from "@angular/common";
 import {GenericButtonModule} from "generic-buttons";
+import {RouterOutlet} from "@angular/router";
 
 
 @NgModule({
@@ -9,6 +10,7 @@ import {GenericButtonModule} from "generic-buttons";
     TwentyfiveModalComponent,
   ],
   imports: [
+    RouterOutlet,
     NgClass,
     GenericButtonModule,
     NgIf,
