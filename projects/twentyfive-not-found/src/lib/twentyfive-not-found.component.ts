f78dd9e1193225e5cfe3e80742b7279f8d8f44d2
Simone Bria <simone.bria@iphonedeveloper.it>
2024-01-29 11:12:00 +0100

    updated spinner, not-found

--- projects/twentyfive-not-found/src/lib/twentyfive-not-found.component.ts
+++ projects/twentyfive-not-found/src/lib/twentyfive-not-found.component.ts
@@ -6,7 +6,7 @@ import {ButtonSizeTheme, ButtonTheme} from "twentyfive-style";
   templateUrl: `./twentyfive-not-found.component.html`
 })
 export class TwentyfiveNotFoundComponent {
-  @Input() imageUrl: string = "assets/404.svg";
+  @Input() imageUrl: string = "assets/images/404.svg";
   @Input() title: string = "Oops! Sembra che tu abbia smarrito la strada";
   @Input() subtitle: string = "Sei finito in una zona digitale sconosciuta, ma non preoccuparti, siamo qui per aiutarti a ritrovare la via!";
 
