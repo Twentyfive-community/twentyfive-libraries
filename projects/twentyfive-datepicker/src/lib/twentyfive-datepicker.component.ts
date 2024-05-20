c5d2a843ec04a2e70684802752606146f9916aa3
loscls <lorenzo.j.scalise@gmail.com>
2024-05-20 18:44:34 +0200

    added setMinDateToToday and list of disabled dates

--- projects/twentyfive-datepicker/src/lib/twentyfive-datepicker.component.ts
+++ projects/twentyfive-datepicker/src/lib/twentyfive-datepicker.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TranslationWidth} from "@angular/common";
 import {DatePickerButtonTheme, DatePickerTheme, LabelTheme } from 'twentyfive-style';
+import {NgbCalendar, NgbDate, NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -8,7 +9,7 @@ import {DatePickerButtonTheme, DatePickerTheme, LabelTheme } from 'twentyfive-st
   templateUrl: './twentyfive-datepicker.component.html',
   styleUrls: ['./twentyfive-datepicker.component.scss']
 })
-export class TwentyfiveDatepickerComponent {
+export class TwentyfiveDatepickerComponent implements OnInit{
 
   @Input() labelText: string = 'Label';
   @Input() placeholder: string = 'dd/mm/yyyy';
@@ -31,6 +32,27 @@ export class TwentyfiveDatepickerComponent {
 
   @Output() dateSelected = new EventEmitter<Date>();
   @Input()  selectedDate: Date | undefined;
+
+  @Input() setMinDateToToday: boolean = false;
+
+  @Input() disabledDates: NgbDate[] = [];
+
+  minDate: NgbDateStruct = new NgbDate(0, 0, 0);
+
+  constructor(private calendar: NgbCalendar) {
+  }
+
+  ngOnInit(): void {
+    if (this.setMinDateToToday) {
+      this.minDate = this.calendar.getToday();
+    }
+  }
+
+  markDisabled = (date: NgbDate): boolean => {
+    return this.disabledDates.some(disabledDate =>
+      date.equals(disabledDate)
+    );
+  };
 
   onDateSelect(event: any) {
     const selectedDate = new Date(event.year, event.month - 1, event.day);
