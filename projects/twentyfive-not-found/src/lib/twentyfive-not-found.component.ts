1291032ab9cc701d7e822bc1282063cbeec7072c
SimoneBria <simone.bria@dxc.com>
2025-02-26 17:45:00 +0100

    twentyfive-not-found.component new version configurable

--- projects/twentyfive-not-found/src/lib/twentyfive-not-found.component.ts
+++ projects/twentyfive-not-found/src/lib/twentyfive-not-found.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ButtonSizeTheme, ButtonTheme} from "twentyfive-style";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'lib-twentyfive-not-found',
   templateUrl: `./twentyfive-not-found.component.html`
 })
-export class TwentyfiveNotFoundComponent {
+export class TwentyfiveNotFoundComponent implements OnInit {
   @Input() imageUrl: string = "assets/images/404.svg";
   @Input() title: string = "Oops! Sembra che tu abbia smarrito la strada";
   @Input() subtitle: string = "Sei finito in una zona digitale sconosciuta, ma non preoccuparti, siamo qui per aiutarti a ritrovare la via!";
@@ -14,4 +15,17 @@ export class TwentyfiveNotFoundComponent {
 
   protected readonly ButtonTheme = ButtonTheme;
   protected readonly ButtonSizeTheme = ButtonSizeTheme;
+
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.data.subscribe((data: any) => {
+      this.imageUrl = data.imageUrl || this.imageUrl;
+      this.title = data.title || this.title;
+      this.subtitle = data.subtitle || this.subtitle;
+      this.routerLink = data.routerLink || this.routerLink;
+    });
+  }
 }
